e53d68f665947f7d76b447521bc34074a87e468d
stephane <stephane.betton@gmail.com>
2023-03-22 18:11:19 +0100

    feat: fizzBuzz
    refacto test

--- business/src/test/java/org/hussard/memo/business/services/katas/fizzbuzz/KataFizzBuzzServiceTest.java
+++ business/src/test/java/org/hussard/memo/business/services/katas/fizzbuzz/KataFizzBuzzServiceTest.java
@@ -2,48 +2,31 @@ package org.hussard.memo.business.services.katas.fizzbuzz;
 
 import org.assertj.core.api.Assertions;
 import org.hussard.memo.business.adapter.in.KataFizzBuzz;
-import org.junit.jupiter.api.Test;
- class KataFizzBuzzServiceTest {
-    @Test
-    void fizzBuzz_checkOne_ReturnOne(){
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class KataFizzBuzzServiceTest {
+
+     private static Stream<Arguments> provideStepOne() {
+         return Stream.of(
+                 Arguments.of(1, "1"),
+                 Arguments.of(2, "2"),
+                 Arguments.of(3, "Fizz"),
+                 Arguments.of(4 , "4"),
+                 Arguments.of(5 , "Buzz"),
+                 Arguments.of(6 , "Fizz"),
+                 Arguments.of(15 , "FizzBuzz")
+         );
+     }
+    @ParameterizedTest(name = "{index} : [input]:{0} => [ouput] {1}")
+    @MethodSource("provideStepOne")
+    void fizzBuzz_StepOne(int value, String result){
         KataFizzBuzz kata = new KataFizzBuzzService();
-        String actual = kata.fizzBuzz(1);
-        Assertions.assertThat(actual).isNotBlank().isEqualTo("1");
+        String actual = kata.fizzBuzz(value);
+        Assertions.assertThat(actual).isNotBlank().isEqualTo(result);
     }
-     @Test
-     void fizzBuzz_checkTwo_ReturnTwo(){
-         KataFizzBuzz kata = new KataFizzBuzzService();
-         String actual = kata.fizzBuzz(2);
-         Assertions.assertThat(actual).isNotBlank().isEqualTo("2");
-     }
-     @Test
-     void fizzBuzz_checkTree_ReturnFizz(){
-         KataFizzBuzz kata = new KataFizzBuzzService();
-         String actual = kata.fizzBuzz(3);
-         Assertions.assertThat(actual).isNotBlank().isEqualTo("Fizz");
-     }
-     @Test
-     void fizzBuzz_checkFour_ReturnFour(){
-         KataFizzBuzz kata = new KataFizzBuzzService();
-         String actual = kata.fizzBuzz(4);
-         Assertions.assertThat(actual).isNotBlank().isEqualTo("4");
-     }
-     @Test
-     void fizzBuzz_checkFive_ReturnBuzz(){
-         KataFizzBuzz kata = new KataFizzBuzzService();
-         String actual = kata.fizzBuzz(5);
-         Assertions.assertThat(actual).isNotBlank().isEqualTo("Buzz");
-     }
-     @Test
-     void fizzBuzz_checkSix_ReturnFizz(){
-         KataFizzBuzz kata = new KataFizzBuzzService();
-         String actual = kata.fizzBuzz(6);
-         Assertions.assertThat(actual).isNotBlank().isEqualTo("Fizz");
-     }
-     @Test
-     void fizzBuzz_checkFifteen_ReturnFizzBuzz(){
-         KataFizzBuzz kata = new KataFizzBuzzService();
-         String actual = kata.fizzBuzz(15);
-         Assertions.assertThat(actual).isNotBlank().isEqualTo("FizzBuzz");
-     }
+
 }
